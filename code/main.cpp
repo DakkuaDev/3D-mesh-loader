b3aa21e1a6e5be33017ec323de40fc18429aa470
dakkua <daniel.guerra.gallardo99@gmail.com>
2021-06-25 20:53:01 +0200

    first commit

---
+++ code/main.cpp
@@ -0,0 +1,47 @@
+/*
+* @Author: Daniel Guerra Gallardo
+* @Description: Programa de renderizado 3D de mayas poligonales
+* @Date: 06/2021
+*/
+
+#include "View.hpp"
+#include <SFML/Window.hpp>
+
+using namespace sf;
+using namespace example;
+
+int main ()
+{
+    // Create the window and the view that will be shown within the window:
+
+    constexpr auto window_width  = 800u;
+    constexpr auto window_height = 600u;
+
+    Window window(VideoMode(window_width, window_height), "Mesh Loader", Style::Titlebar | Style::Close);
+    View   view  (window_width, window_height);
+
+    window.setVerticalSyncEnabled (true);
+
+    // Run the main loop:
+
+    bool exit = false;
+
+    do
+    {
+        Event event;
+
+        while (window.pollEvent (event))
+        {
+            if (event.type == Event::Closed) exit = true;
+        }
+
+        view.update ();
+
+        view.render ();
+
+        window.display ();
+    }
+    while (not exit);
+
+    return 0;
+}
